d46adb2362375e3057fa98b8daeb441f4845b734
0_INTERN_Beatrice Popa <bpopa@noriskshop.de>
2020-06-19 19:21:53 +0200

    https://poc.noriskshop.de/browse/APM-14: payment methods should not be overwritten if module activated

--- Core/Events.php
+++ Core/Events.php
@@ -18,12 +18,121 @@
 namespace Arvato\AfterpayModule\Core;
 
 use OxidEsales\Eshop\Core\DatabaseProvider;
+use OxidEsales\Eshop\Core\DbMetaDataHandler;
 
 /**
  * Class ArvatoAfterpayEvents: Events for the afterpay module.
  */
 class Events
 {
+    private static $createArvatoAfterPay = "CREATE TABLE IF NOT EXISTS `arvatoafterpayafterpayorder` (
+              `OXID` varchar(32) NOT NULL COMMENT 'oxorder.oxorderid',
+              `APRESERVATIONID` varchar(64) NOT NULL,
+              `APCHECKOUTID` varchar(64) NOT NULL,
+              `APCAPTURENO` varchar(64) NOT NULL,
+              `APCAPTURETIMESTAMP` datetime,
+              `APSTATUS` enum('authorized','authorizationvoided','captured','refunded') NOT NULL,
+              `OXTIMESTAMP` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+              `APUSEDAPIKEY` VARCHAR(64) NOT NULL,
+              PRIMARY KEY (`OXID`)
+            ) ENGINE='InnoDB' DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;";
+
+    /**
+     * An array of SQL statements, that will be executed only at the first time of module installation.
+     *
+     * @var array
+     */
+    private static $replaceArvatoAfterPaySQLs = [
+        "REPLACE INTO `oxpayments` (`OXID`, `OXACTIVE`, `OXDESC`, `OXADDSUM`, `OXADDSUMTYPE`, `OXADDSUMRULES`, `OXFROMBONI`, `OXFROMAMOUNT`, `OXTOAMOUNT`, `OXVALDESC`, `OXCHECKED`, `OXDESC_1`, `OXVALDESC_1`, `OXDESC_2`, `OXVALDESC_2`, `OXDESC_3`, `OXVALDESC_3`, `OXLONGDESC`, `OXLONGDESC_1`, `OXLONGDESC_2`, `OXLONGDESC_3`, `OXSORT`, `OXTIMESTAMP`) VALUES
+
+              ('afterpaydebitnote',	1,	'AfterPay Lastschrift',	0,	'abs',	0,	0,	0,	1000000,	'apdebitbankaccount__@@apdebitbankcode__@@apbirthday__@@apphone__@@apssn__@@',	0,	'AfterPay Direct Debit',
+              'apdebitbankaccount__@@apdebitbankcode__@@apbirthday__@@apphone__@@apssn__@@',	'',	'',	'',	'',	'','',	'',	'',	3,	'2017-11-08 11:48:51'),
+
+              ('afterpayinvoice',	1,	'AfterPay Rechnung',	0,	'abs',	0,	0,	0,	1000000,	'apbirthday__@@apphone__@@apssn__@@',	0,	'AfterPay Invoice',	'apbirthday__@@apphone__@@apssn__@@',	'',	'',	'',	'',	'',	'',	'',		2,	'',	'2017-11-08 11:48:51'),
+
+              ('afterpayinstallment',	1,	'AfterPay Ratenzahlung',	0,	'abs',	0,	0,	0,	1000000,	'apinstallmentbankaccount__@@apinstallmentbankcode__@@afterpayInstallmentProfileId__@@apbirthday__@@apphone__@@apssn__@@',	0,	'AfterPay Installment',
+              'apinstallmentbankaccount__@@apinstallmentbankcode__@@afterpayInstallmentProfileId__@@apbirthday__@@apphone__@@apssn__@@',	'',	'',	'',	'','','','',	1,	'',	'2017-11-08 11:48:51');",
+
+        "REPLACE INTO `oxobject2payment` (`OXID`, `OXPAYMENTID`, `OXOBJECTID`, `OXTYPE`, `OXTIMESTAMP`) VALUES
+              ('apinv2standard',	'afterpayinvoice',	'oxidstandard',	'oxdelset',	'2017-11-08 11:48:51'),
+              ('apdebit2standard',	'afterpaydebitnote',	'oxidstandard',	'oxdelset',	'2017-11-08 11:48:51'),
+              ('apinstallment2standard',	'afterpayinstallment',	'oxidstandard',	'oxdelset',	'2017-11-08 11:48:51'),
+
+              ('apinv2germany',	'afterpayinvoice',	'a7c40f631fc920687.20179984',	'oxcountry',	'2017-11-08 11:48:51'),
+              ('apdebit2germany',	'afterpaydebitnote',	'a7c40f631fc920687.20179984',	'oxcountry',	'2017-11-08 11:48:51'),
+              ('apinv2nl',	'afterpayinvoice',	'a7c40f632cdd63c52.64272623',	'oxcountry',	'2017-11-08 11:48:51'),
+              ('apdebit2nl',	'afterpaydebitnote',	'a7c40f632cdd63c52.64272623',	'oxcountry',	'2017-11-08 11:48:51'),
+              ('apinstallment2germany',	'afterpayinstallment',	'a7c40f631fc920687.20179984',	'oxcountry',	'2017-11-08 11:48:51');"
+    ];
+
+    /**
+     * Check if field exists in table
+     *
+     * @param string $fieldName field name
+     * @param string $tableName table name
+     *
+     * @return bool
+     */
+    protected static function fieldExists($fieldName, $tableName)
+    {
+        $dbMetaDataHandler = oxNew(DbMetaDataHandler::class );
+
+        return $dbMetaDataHandler->fieldExists($fieldName, $tableName);
+    }
+
+    /**
+     * setUpModule
+     * -----------------------------------------------------------------------------------------------------------------
+     * Execute the sqlQueries at the first time of the module installation.
+     *
+     */
+    private static function setUpModule() {
+
+        // Check if the arvatoafterpayorder table was already created, if not create it.
+        if (!self::tableExists('arvatoafterpayafterpayorder')) {
+            self::executeSQL(self::$createArvatoAfterPay);
+        }
+
+        // Check if the tables oxpayments and oxobject2payment have all fields, if not replace them with the standard values
+        foreach (self::$replaceArvatoAfterPaySQLs as $sqlQuery) {
+            // execute the statements for oxpayments und oxobject2payment
+            self::executeSQL($sqlQuery);
+        }
+    }
+
+    /**
+     * Check if table exists
+     *
+     * @param string $sTableName table name
+     *
+     * @return bool
+     */
+    protected static function tableExists($sTableName)
+    {
+        $oDbMetaDataHandler = oxNew(DbMetaDataHandler::class );
+
+        return $oDbMetaDataHandler->tableExists($sTableName);
+    }
+
+    /**
+     * Executes given sql statement.
+     *
+     * @param string $sqlQuery sql query to execute.
+     */
+    private static function executeSQL($sqlQuery)
+    {
+        DatabaseProvider::getDb()->execute($sqlQuery);
+    }
+
+    /**
+     * Regenerate views for changed tables
+     */
+    protected static function regenerateViews()
+    {
+        $oDbMetaDataHandler = oxNew(DbMetaDataHandler::class );
+        $oDbMetaDataHandler->updateViews();
+    }
+
     /**
      * On module activation callback
      */
@@ -32,12 +141,26 @@ class Events
         $db = DatabaseProvider::getDb();
         $logging = oxNew(\Arvato\AfterpayModule\Core\Logging::class);
 
-        foreach (self::getSQLs() as $sql) {
-            try {
-                $db->execute($sql);
-            } catch (\Exception $e) {
-                 $logging->logInstallation('SQL-ERROR ON ACTIVATE: ' . $e->getMessage());
+        $selectPaymentTypes = "SELECT OXID FROM oxpayments WHERE OXID LIKE 'afterpay%'";
+        $resPaymentTypes = $db->getAll($selectPaymentTypes);
+
+        foreach($resPaymentTypes as $resPaymentType) {
+            // check if the payment types are not in database and execute setupModule()
+            if(count($resPaymentTypes) < 3) {
+                self::setupModule();
             }
+            else {
+                // if the payment types are already in the database, then update the values
+                if(count($resPaymentTypes) == 3) {
+                    $paymentTable = oxNew(\OxidEsales\Eshop\Application\Model\Payment::class);
+
+                    foreach($resPaymentType as $keyPay=>$payType) {
+                        $paymentTable->load($payType);
+                    }
+                    $paymentTable->save();
+                }
+            }
+            self::regenerateViews();
         }
 
         // Get column if exist
@@ -100,43 +223,6 @@ class Events
     {
     }
 
-    protected static function getSQLs()
-    {
-        return [
-            "CREATE TABLE IF NOT EXISTS `arvatoafterpayafterpayorder` (
-              `OXID` varchar(32) NOT NULL COMMENT 'oxorder.oxorderid',
-              `APRESERVATIONID` varchar(64) NOT NULL,
-              `APCHECKOUTID` varchar(64) NOT NULL,
-              `APCAPTURENO` varchar(64) NOT NULL,
-              `APCAPTURETIMESTAMP` datetime,
-              `APSTATUS` enum('authorized','authorizationvoided','captured','refunded') NOT NULL,
-              `OXTIMESTAMP` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-              `APUSEDAPIKEY` VARCHAR(64) NOT NULL,
-              PRIMARY KEY (`OXID`)
-            ) ENGINE='InnoDB' DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;",
-
-            "REPLACE INTO `oxpayments` (`OXID`, `OXACTIVE`, `OXDESC`, `OXADDSUM`, `OXADDSUMTYPE`, `OXADDSUMRULES`, `OXFROMBONI`, `OXFROMAMOUNT`, `OXTOAMOUNT`, `OXVALDESC`, `OXCHECKED`, `OXDESC_1`, `OXVALDESC_1`, `OXDESC_2`, `OXVALDESC_2`, `OXDESC_3`, `OXVALDESC_3`, `OXLONGDESC`, `OXLONGDESC_1`, `OXLONGDESC_2`, `OXLONGDESC_3`, `OXSORT`, `OXTIMESTAMP`) VALUES
-
-              ('afterpaydebitnote',	1,	'AfterPay Lastschrift',	0,	'abs',	0,	0,	0,	1000000,	'apdebitbankaccount__@@apdebitbankcode__@@apbirthday__@@apphone__@@apssn__@@',	0,	'AfterPay Direct Debit',
-              'apdebitbankaccount__@@apdebitbankcode__@@apbirthday__@@apphone__@@apssn__@@',	'',	'',	'',	'',	'','',	'',	'',	3,	'2017-11-08 11:48:51'),
-
-              ('afterpayinvoice',	1,	'AfterPay Rechnung',	0,	'abs',	0,	0,	0,	1000000,	'apbirthday__@@apphone__@@apssn__@@',	0,	'AfterPay Invoice',	'apbirthday__@@apphone__@@apssn__@@',	'',	'',	'',	'',	'',	'',	'',		2,	'',	'2017-11-08 11:48:51'),
-
-              ('afterpayinstallment',	1,	'AfterPay Ratenzahlung',	0,	'abs',	0,	0,	0,	1000000,	'apinstallmentbankaccount__@@apinstallmentbankcode__@@afterpayInstallmentProfileId__@@apbirthday__@@apphone__@@apssn__@@',	0,	'AfterPay Installment',
-              'apinstallmentbankaccount__@@apinstallmentbankcode__@@afterpayInstallmentProfileId__@@apbirthday__@@apphone__@@apssn__@@',	'',	'',	'',	'','','','',	1,	'',	'2017-11-08 11:48:51');",
-
-            "REPLACE INTO `oxobject2payment` (`OXID`, `OXPAYMENTID`, `OXOBJECTID`, `OXTYPE`, `OXTIMESTAMP`) VALUES
-              ('apinv2standard',	'afterpayinvoice',	'oxidstandard',	'oxdelset',	'2017-11-08 11:48:51'),
-              ('apdebit2standard',	'afterpaydebitnote',	'oxidstandard',	'oxdelset',	'2017-11-08 11:48:51'),
-              ('apinstallment2standard',	'afterpayinstallment',	'oxidstandard',	'oxdelset',	'2017-11-08 11:48:51'),
-
-              ('apinv2germany',	'afterpayinvoice',	'a7c40f631fc920687.20179984',	'oxcountry',	'2017-11-08 11:48:51'),
-              ('apdebit2germany',	'afterpaydebitnote',	'a7c40f631fc920687.20179984',	'oxcountry',	'2017-11-08 11:48:51'),
-              ('apinv2nl',	'afterpayinvoice',	'a7c40f632cdd63c52.64272623',	'oxcountry',	'2017-11-08 11:48:51'),
-              ('apdebit2nl',	'afterpaydebitnote',	'a7c40f632cdd63c52.64272623',	'oxcountry',	'2017-11-08 11:48:51'),
-              ('apinstallment2germany',	'afterpayinstallment',	'a7c40f631fc920687.20179984',	'oxcountry',	'2017-11-08 11:48:51');"
-        ];
-    }
 
     protected static function getShopSpecificSQLs()
     {
