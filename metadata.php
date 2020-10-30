626f5585f2c90a6160747b428af357a0e5b8898e
ckurz <ckurz@noriskshop.de>
2020-10-30 09:53:58 +0100

    change metadata

--- metadata.php
+++ metadata.php
@@ -34,8 +34,8 @@ $aModule = [
         'en' => 'standalone release of the Arvato AfterPay Module'
     ],
     'thumbnail'   => 'Application/views/out/img/AfterPay_logo_green.png',
-    'version' => '2.1.0-beta.2',
-    'author' => 'OXID eSales AG',
+    'version' => '2.1.0-beta.3',
+    'author' => 'norisk GmbH',
     'url' => '',
     'email' => '',
     'events' => [
