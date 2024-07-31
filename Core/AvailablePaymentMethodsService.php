0d60999b6f248f6182d326f8ef5263476e656a96
ssiepmann <ssiepmann@noriskshop.de>
2024-07-31 10:22:11 +0200

    https://poc.noriskshop.de/browse/APM-65 fix: remove return type in signature

--- Core/AvailablePaymentMethodsService.php
+++ Core/AvailablePaymentMethodsService.php
@@ -208,7 +208,7 @@ class AvailablePaymentMethodsService extends \Arvato\AfterpayModule\Core\Service
      * @return bool|array
      * @throws CurlException
      */
-    public function getAvailableInstallmentPlans(): mixed
+    public function getAvailableInstallmentPlans()
     {
         if ($this->installmentPaymentActive()) {
             $amount = $this->_session->getBasket()->getPrice()->getBruttoPrice();
