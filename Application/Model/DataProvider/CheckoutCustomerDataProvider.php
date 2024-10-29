7f02afcd8be3f9f39e047146414b1b9d6447b856
ssiepmann <ssiepmann@noriskshop.de>
2024-10-29 13:57:43 +0100

    https://poc.noriskshop.de/browse/APM-66 fix: revert dynvalues

--- Application/Model/DataProvider/CheckoutCustomerDataProvider.php
+++ Application/Model/DataProvider/CheckoutCustomerDataProvider.php
@@ -56,8 +56,8 @@ class CheckoutCustomerDataProvider extends \Arvato\AfterpayModule\Application\Mo
         // Phone
 
         ($phone = $user->oxuser__oxmobfon->value) || ($phone = $user->oxuser__oxfon->value) || ($phone = $user->oxuser__oxprivfon->value);
-        if (isset($dynValues['apfon'])) {
-            $phone = $dynValues['apfon'];
+        if (isset($dynValues['apfon'][$paymentId])) {
+            $phone = $dynValues['apfon'][$paymentId];
         }
         $dataObject->setPhone($phone);
 
@@ -65,8 +65,8 @@ class CheckoutCustomerDataProvider extends \Arvato\AfterpayModule\Application\Mo
 
         $birthdate = $user->oxuser__oxbirthdate->value;
 
-        if (isset($dynValues['apbirthday'])) {
-            $birthdate = $dynValues['apbirthday'];
+        if (isset($dynValues['apbirthday'][$paymentId])) {
+            $birthdate = $dynValues['apbirthday'][$paymentId];
 
             // Target: yyyy-mm-dd
             $birthdate = $this->_prepareBirthDate($birthdate);
@@ -78,8 +78,8 @@ class CheckoutCustomerDataProvider extends \Arvato\AfterpayModule\Application\Mo
 
         // SSN
 
-        if (isset($dynValues['apssn'])) {
-            $ssn = $dynValues['apssn'];
+        if (isset($dynValues['apssn'][$paymentId])) {
+            $ssn = $dynValues['apssn'][$paymentId];
             $dataObject->setIdentificationNumber($ssn);
         }
 
